512a005c788e3ff79ed9ce57ed79c4c102a8998b
ElinPettersson <82908619+ElinPettersson@users.noreply.github.com>
2021-09-24 12:05:25 +0200

    For loop and try catch
    Update with a for loop and a try catch

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var a;
 function show_hide() {
+
     if (a == 1) {
         document.getElementById("poffis").style.display = "inline";
         return a = 0;
@@ -9,3 +10,27 @@ function show_hide() {
         return a = 1;
     }
 }
+
+
+
+
+
+
+
+
+for (var counter = 0; counter < 10; counter++) {
+    console.log("POFFIS");
+}
+
+
+
+
+
+
+try {
+    alllllert("HEJA GNAGET!");
+}
+catch (err) {
+    document.getElementById("testError").innerHTML = err.message;
+}
+
